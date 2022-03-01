653916a70ac1d4f0f980cbd8359572685f9f70a8
Thonoton <73487128+Thonoton@users.noreply.github.com>
2022-02-28 16:31:46 -0800

    add Constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -12,4 +12,32 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+public final class Constants {
+    public final static double MULTI(double speed, double sensitivity){
+        double multiOutput = Math.pow(speed, 3) * sensitivity + (1 - sensitivity) * speed;
+        return multiOutput;
+    }
+
+    public final static double SENSITIVITY = 1;
+
+    /* Joystick */
+    public static final int DRIVE_JOYSTICK_PORT = 1;
+
+    /* Drive Train Motors */
+    public static final int LEFT_FRONT_TOP_MOTOR = 1;
+    public static final int LEFT_FRONT_BOTTOM_MOTOR = 2;
+    public static final int LEFT_BACK_TOP_MOTOR = 3;
+    public static final int LEFT_BACK_BOTTOM_MOTOR = 4;
+
+    public static final int RIGHT_FRONT_TOP_MOTOR = 5;
+    public static final int RIGHT_FRONT_BOTTOM_MOTOR = 6;
+    public static final int RIGHT_BACK_TOP_MOTOR = 7;
+    public static final int RIGHT_BACK_BOTTOM_MOTOR = 8;
+
+    /* Encoders */
+    public static final int[] LEFT_ENCODER= new int[]{0,1};
+    public static final int[] RIGHT_ENCODER = new int[]{2,3};
+
+    public static final double WHEELPERIMETERCM = 15.24 * Math.PI;
+    public static final double DISTANCEPERPULS = WHEELPERIMETERCM / 2048;
+}
