4a98b98bdc16b8f860f255f8d08d500ca846e090
Brennan Wong <1304339@edu.burnabyschools.ca>
2022-03-01 16:06:15 -0800

    added drivetrain subsystem with a drivetrain command that uses a joystick

--- src/src/main/java/frc/robot/Constants.java
+++ src/src/main/java/frc/robot/Constants.java
@@ -12,4 +12,21 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+public final class Constants {
+    public static final int LEFT_FRONT_TOP_MOTOR = 5;
+    public static final int LEFT_FRONT_BOTTOM_MOTOR = 10;
+    public static final int LEFT_BACK_TOP_MOTOR = 13;
+    public static final int LEFT_BACK_BOTTOM_MOTOR = 8;
+
+    public static final int RIGHT_FRONT_TOP_MOTOR = 1;
+    public static final int RIGHT_FRONT_BOTTOM_MOTOR = 11;
+    public static final int RIGHT_BACK_TOP_MOTOR = 12;
+    public static final int RIGHT_BACK_BOTTOM_MOTOR = 4;
+
+    public static final int[] LEFT_ENCODER= new int[]{0,1};
+    public static final int[] RIGHT_ENCODER = new int[]{2,3};
+
+    public static final double WHEELPERIMETERCM = 15.24*Math.PI;
+    public static final double DISTENCEPERPULS = WHEELPERIMETERCM/2048;
+
+}
