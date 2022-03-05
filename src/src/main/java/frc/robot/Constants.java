45b1ad9961e0fb5511ad3f1b3d3a7d716c3cba1e
Ryutaro623 <ryutarouonozawa@icloud.com>
2022-03-05 01:16:20 -0800

    added constants

--- src/src/main/java/frc/robot/Constants.java
+++ src/src/main/java/frc/robot/Constants.java
@@ -13,6 +13,7 @@ package frc.robot;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
+    //Drive train 
     public static final int LEFT_FRONT_TOP_MOTOR = 5;
     public static final int LEFT_FRONT_BOTTOM_MOTOR = 10;
     public static final int LEFT_BACK_TOP_MOTOR = 13;
@@ -28,5 +29,13 @@ public final class Constants {
 
     public static final double WHEELPERIMETERCM = 15.24*Math.PI;
     public static final double DISTENCEPERPULS = WHEELPERIMETERCM/2048;
+
+    //Arm
+    public static final int[] MOTOR_FOR_EXTENDING = new int[]{1,3};
+    public static final int TOP_LIMIT_SWITCH = 0;
+    public static final int BOTTOM_LIMIT_SWITCH = 1;
+
+    //Pneumatic
+    public static final int[] DOUBLE_SOLENOID = new int[]{0,1};
 
 }
--- src/src/main/java/frc/robot/subsystems/MotorExtender.java
+++ src/src/main/java/frc/robot/subsystems/MotorExtender.java
@@ -4,11 +4,22 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
 public class MotorExtender extends SubsystemBase {
   /** Creates a new MotorExtender. */
+  private final TalonSRX motor_a = new TalonSRX(Constants.MOTOR_FOR_EXTENDING[0]);
+  private final TalonSRX motor_b = new TalonSRX(Constants.MOTOR_FOR_EXTENDING[1]);
   public MotorExtender() {}
+
+  public void run(double speed){
+    motor_a.set(ControlMode.PercentOutput, speed);
+    motor_b.set(ControlMode.PercentOutput, speed);
+  }
 
   @Override
   public void periodic() {
